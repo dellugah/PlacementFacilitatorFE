745f2060c0d3d32c1773de5f0ccd1083488e0979
Andre Machado <andreluiz.dagamamachado@student.sl.on.ca>
2025-05-09 15:42:51 -0300

    Refactor auth flow and enhance routing structure
    Replaced BehaviorSubject-based token handling with sessionStorage APIs for better state management. Added child routing for the employer and student sections and updated corresponding components. Improved UI styles and linked router navigation in employer profile for better UX.

---
+++ src/app/components/employer/placements/placements.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-placements',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './placements.component.html',
+  standalone: true,
+  styleUrl: './placements.component.css'
+})
+export class PlacementsComponent {
+
+}
+
+export class placements {
+}
---
+++ src/app/components/student-profile/student-profile/student-profile.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {ProfileDTO} from '../../../DTOs/ProfileDTO';
+import {ConnectionService} from '../../../services/connection/connection.service';
+import {ProfileServiceService} from '../../../services/profileServices/profile-service.service';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-student-profile',
+  imports: [
+    NgOptimizedImage,
+    RouterLink,
+    RouterOutlet
+  ],
+  templateUrl: './student-profile.component.html',
+  standalone: true,
+  styleUrl: './student-profile.component.css'
+})
+export class StudentProfileComponent implements OnInit{
+
+  profile$: BehaviorSubject<ProfileDTO> = new BehaviorSubject<ProfileDTO>(new ProfileDTO());
+
+  constructor(private connection : ConnectionService,
+              protected profile : ProfileServiceService) { }
+
+  async ngOnInit(){
+    const loginData = await this.connection.getLogIn();
+    this.profile.profile = new BehaviorSubject<ProfileDTO>(loginData as ProfileDTO);
+    this.profile$ = this.profile.profile;
+  }
+
+}
